ba465a45db349350c61a4a2b7e4212dca6e21ab4
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-19 19:55:02 -0300

    upgrade imgens e progress html e css

---
+++ script.js
@@ -0,0 +1,24 @@
+function btn_cx1() {
+    alert("Jonh foi clicado");
+}
+
+function btn_cx2() {
+    alert("Striani foi clicado!");
+}
+
+function btn_cx3() {
+    alert("Kiko foi clicado");
+}
+
+function btn_cx4() {
+    alert("A imagem foi clicada!");
+}
+
+function btn_cx5() {
+    alert("A imagem foi clicada!");
+}
+
+function btn_cx6() {
+    alert("A imagem foi clicada!");
+}
+
