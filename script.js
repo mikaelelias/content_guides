12ccb51c7e36ac85ffb4c9bb83619068bb8c4f49
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-24 11:53:08 -0300

    modal

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function btn_cx1() {
     let modal = document.querySelector(".modal1");
     modal.style.display = "block";
 }
+
+function btn_cx2() {
+    let modal = document.querySelector(".modal2");
+    modal.style.display = "block";
+}
+
+function btn_cx3() {
+    let modal = document.querySelector(".modal3");
+    modal.style.display = "block";
+}
+
+function btn_cx4() {
+    let modal = document.querySelector(".modal4");
+    modal.style.display = "block";
+}
+
+function btn_cx5() {
+    let modal = document.querySelector(".modal5");
+    modal.style.display = "block";
+}
+
+function btn_cx6() {
+    let modal = document.querySelector(".modal6");
+    modal.style.display = "block";
+}
