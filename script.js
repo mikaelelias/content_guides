8d18e1687d35569715145c6dee0f667d3b3c96ce
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-24 11:27:32 -0300

    progresse

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 function btn_cx1() {
-    let modal = document.querySelector(".modal.modal1");
-    modal.classList.add("show-modal");
+    let modal = document.querySelector(".modal1");
+    modal.style.display = "block";
 }
-
-function btn_cx2() {
-    let modal = document.querySelector(".modal.modal2");
-    modal.classList.add("show-modal");
-}
-
-function btn_cx3() {
-    let modal = document.querySelector(".modal.modal3");
-    modal.classList.add("show-modal");
-}
-
-function btn_cx4() {
-    let modal = document.querySelector(".modal.modal4");
-    modal.classList.add("show-modal");
-}
-
-function btn_cx5() {
-    let modal = document.querySelector(".modal.modal5");
-    modal.classList.add("show-modal");
-}
-
-function btn_cx6() {
-    let modal = document.querySelector(".modal.modal6");
-    modal.classList.add("show-modal");
-}
-
