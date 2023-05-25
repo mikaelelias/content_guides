a40524227794997e8b448d7775d5b2dab819f9cd
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-25 17:54:04 -0300

    modal progress css

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function btn_cx1() {
     caixas.classList.add("modal-open");
 }
 
+function btn_close_modal() {
+    let modal = document.querySelector(".modal1");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
+}
+
 function btn_cx2() {
     let modal = document.querySelector(".modal2");
     modal.style.display = "block";
@@ -14,12 +22,29 @@ function btn_cx2() {
     container.classList.add("modal-open");
 }
 
+function btn_close_modal2() {
+    let modal = document.querySelector(".modal2");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
+}
+
+
 function btn_cx3() {
     let modal = document.querySelector(".modal3");
     modal.style.display = "block";
 
     let container = document.querySelector(".container");
     container.classList.add("modal-open");
+}
+
+function btn_close_modal3() {
+    let modal = document.querySelector(".modal3");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
 }
 
 function btn_cx4() {
@@ -30,6 +55,14 @@ function btn_cx4() {
     container.classList.add("modal-open");
 }
 
+function btn_close_modal4() {
+    let modal = document.querySelector(".modal4");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
+}
+
 function btn_cx5() {
     let modal = document.querySelector(".modal5");
     modal.style.display = "block";
@@ -38,10 +71,26 @@ function btn_cx5() {
     container.classList.add("modal-open");
 }
 
+function btn_close_modal5() {
+    let modal = document.querySelector(".modal5");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
+}
+
 function btn_cx6() {
     let modal = document.querySelector(".modal6");
     modal.style.display = "block";
 
     let container = document.querySelector(".container");
     container.classList.add("modal-open");
+}
+
+function btn_close_modal6() {
+    let modal = document.querySelector(".modal6");
+    modal.style.display = "none";
+
+    let container = document.querySelector(".container");
+    container.classList.remove("modal-open");
 }
