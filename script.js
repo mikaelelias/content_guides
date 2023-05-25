d7b8e7bb3eb0b8fee581ad14cc3afcfc6398a2cc
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2023-05-24 22:17:33 -0300

    upgrade

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 function btn_cx1() {
     let modal = document.querySelector(".modal1");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
 
 function btn_cx2() {
     let modal = document.querySelector(".modal2");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
 
 function btn_cx3() {
     let modal = document.querySelector(".modal3");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
 
 function btn_cx4() {
     let modal = document.querySelector(".modal4");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
 
 function btn_cx5() {
     let modal = document.querySelector(".modal5");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
+
 
 function btn_cx6() {
     let modal = document.querySelector(".modal6");
     modal.style.display = "block";
+
+    let container = document.querySelector(".container");
+    container.classList.add("modal-open");
 }
